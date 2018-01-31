d14a12df94d4a228d32fe8703145530ecaaba5ca
Marks Paskannijs <paskannm@aston.ac.uk>
2018-01-31 22:23:20 +0000

    Added helper functions and fields

---
+++ app/src/main/java/uk/ac/aston/paskannm/pockettraveler/helper/AppConstant.java
@@ -0,0 +1,23 @@
+package uk.ac.aston.paskannm.pockettraveler.helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class holds static constants used across application
+ */
+
+public class AppConstant {
+
+    // Number of Columns of Grid View
+    public static final int NUM_OF_COLUMNS = 3;
+
+    // Grid View image padding in dp
+    public static final int GRID_PADDING = 8;
+
+    // SD card image directory
+    public static final String PHOTO_ALBUM = "Camera";
+
+    // Supported file formats
+    public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg", "png");
+}
