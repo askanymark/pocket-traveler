db040dcde8ecdf030b20f17f9a66b4fedf908753
Marks Paskannijs <paskannm@aston.ac.uk>
2018-01-31 21:25:46 +0000

    Resolved #4

--- app/src/main/java/uk/ac/aston/paskannm/pockettraveler/PlacesFragment.java
+++ app/src/main/java/uk/ac/aston/paskannm/pockettraveler/PlacesFragment.java
@@ -1,11 +1,13 @@
 package uk.ac.aston.paskannm.pockettraveler;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class PlacesFragment extends android.support.v4.app.Fragment {
 
@@ -20,5 +22,29 @@ public class PlacesFragment extends android.support.v4.app.Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.view_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                CharSequence text = "You clicked VIEW";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        view.findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                CharSequence text = "You clicked EDIT";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
